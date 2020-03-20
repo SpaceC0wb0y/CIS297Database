396a2805e1a3536785a78871f3d730f78a46b314
MuazAlhaidar <omunam10@gmail.com>
2020-03-20 16:00:43 -0400

    Adding multiple menus so we can work on different aspects of the project at the same time

--- Registration Database/Registration Database/FacultyMenu.cs
+++ Registration Database/Registration Database/FacultyMenu.cs
@@ -9,8 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Registration_Database {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class FacultyMenu : Form {
+        public FacultyMenu() {
             InitializeComponent();
         }
     }
--- Registration Database/Registration Database/StudentMenu.Designer.cs
+++ Registration Database/Registration Database/StudentMenu.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace Registration_Database {
-    partial class Form1 {
+    partial class StudentMenu {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -23,19 +23,12 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.ResumeLayout(false);
-
+            this.Text = "StudentMenu";
         }
 
         #endregion
     }
 }
-
